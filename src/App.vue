d2e355a4c94b98137cdadd17552d27406532c1e6
Yoan <yoanestrada01@gmail.com>
2021-10-09 01:15:14 -0400

    configuracion inicial del proyecto promer commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <h1>Weather App</h1>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
